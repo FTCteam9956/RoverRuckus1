be54965d701183fd92af6a9dd7f9ec4c006b5b5a
Teagan <tbrohman@gmail.com>
2018-11-20 17:42:59 -0600

    Added Controller Support and Auto Classes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RoverRuckus/RoverTeleopQualifier.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RoverRuckus/RoverTeleopQualifier.java
@@ -15,6 +15,8 @@ public class RoverTeleopQualifier extends LinearOpMode{
     public RoverHardware robot = new RoverHardware();
 
   public int OBJ = 2; // # is in CM
+    public float leftPower;
+    public float rightPower;
 
     public void runOpMode(){
         robot.init(hardwareMap);
@@ -22,11 +24,23 @@ public class RoverTeleopQualifier extends LinearOpMode{
         waitForStart();
 
         while(opModeIsActive()){
-            robot.left1.setPower(gamepad1.left_stick_y * .3);
-            robot.left2.setPower(gamepad1.left_stick_y * .3);
-            robot.right1.setPower(gamepad1.right_stick_y * .3);
-            robot.right2.setPower(gamepad1.right_stick_y * .3);
+//            robot.left1.setPower(gamepad1.left_stick_y * .3);
+//            robot.left2.setPower(gamepad1.left_stick_y * .3);
+//            robot.right1.setPower(gamepad1.right_stick_y * .3);
+//            robot.right2.setPower(gamepad1.right_stick_y * .3);
 
+            //Drive Motors
+            leftPower = (gamepad2.left_stick_y + gamepad2.left_stick_x);
+            rightPower = (gamepad2.left_stick_y - gamepad2.left_stick_x);
+            //Tank Drive
+            robot.left1.setPower(-leftPower / 3);
+
+            robot.right1.setPower(-rightPower / 3);
+
+            //Moves the Bopper In and Out
+            robot.bop.setPower(gamepad1.right_stick_y * 0.5);
+
+            // Hang Mechanism
           if (gamepad1.dpad_up){
               robot.hang.setPower(-1);
           } else if (gamepad1.dpad_down){
@@ -34,13 +48,27 @@ public class RoverTeleopQualifier extends LinearOpMode{
           } else{
               robot.hang.setPower(0);
           }
-
-          if (gamepad1.x){
+            //Activates the Launcher
+          if (gamepad1.b){
                 robot.launcher.setPower(1);
                 sleep(250);
                 robot.launcher.setPower(0);
             }
-
+            //Sets Servo Position to Top
+          if (gamepad1.y) {
+                robot.drop.setPosition(1);
+          }
+            //Sets Servo Position to Bottom
+          if(gamepad1.a){
+              robot.drop.setPosition(0);
+          }
+          //Rotates the Bopper
+          if(gamepad1.right_trigger <= 0.5){
+              robot.rotateMech.setPower(gamepad1.right_trigger * 0.5);
+          }
+          if (gamepad1.left_trigger <= 0.5){
+              robot.rotateMech.setPower(-gamepad1.right_trigger * 0.5);
+          }
 
 
             //Telemetry Section
